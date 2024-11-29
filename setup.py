b45ca63132ba0b9d859aeed60f60f40b24011fff
Christian Drischler <drischler@ohio.edu>
2024-11-29 11:17:18 -0500

    adds check for GSL in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import subprocess
+
+try:
+    result = subprocess.run(["gsl-config", "--prefix"], capture_output=True, text=True)
+    print(f"GSL found in '{result.stdout}'")
+except FileNotFoundError:
+    print(f"Warning: GSL is not installed. Build will not work.")
 
 ext_modules = [Extension('chiralPot', ['cpot.pyx'],
                          libraries=['localGt+', 'gsl', 'blas'],
-                         library_dirs=['.', '/usr/local/lib', '/opt/homebrew/Cellar/gsl/2.7.1/lib'], language='c++')]
+                         library_dirs=['.', '/usr/local/lib', '/opt/homebrew/lib'], language='c++')]
 
 setup(name = 'chiral potential extension module',
       cmdclass = {'build_ext': build_ext},
