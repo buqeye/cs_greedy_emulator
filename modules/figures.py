3654b90d20338539fcfdafd91ca9a7c9f018a8c7
Christian Drischler <drischler@frib.msu.edu>
2025-03-27 13:30:30 -0400

    improves data management

--- modules/figures.py
+++ modules/figures.py
@@ -23,6 +23,7 @@ class convergenceAnalysis:
                  which="K",
                  seed=None):
         self.snapshot_range = snapshot_range
+        self.E_MeV = E_MeV
         self.num_sample = num_sample
         self.inhomogeneous = inhomogeneous
         self.emulator_type = emulator_type
@@ -127,8 +128,8 @@ class convergenceAnalysis:
             tmp = pod_emul.emulate(self.lecs_list_validation, 
                                    mode=self.emulator_type, which=self.which)
             assert pod_emul.snapshot_matrix.shape[1] == num_snapshots, "something's wrong in the POD emulator"
-            df_tmp = pd.DataFrame(data={"approach": "POD", "num_snapshots": num_snapshots, 
-                                        "error": self.y_axis_quantity(tmp)})
+            df_tmp = pd.DataFrame(data={"approach": "POD", "num_snapshots": num_snapshots, "E_MeV": self.E_MeV,
+                                        "error": self.y_axis_quantity(tmp), "emulator_type": self.emulator_type})
             df = pd.concat((df, df_tmp))
         return df if df_out is None else pd.concat((df_out, df))
 
@@ -148,8 +149,8 @@ class convergenceAnalysis:
                 tmp = lhs_emul.emulate(self.lecs_list_validation, 
                                     mode=self.emulator_type, which=self.which)
                 assert lhs_emul.snapshot_matrix.shape[1] == num_snapshots, "something's wrong in the LHS emulator"
-                df_tmp = pd.DataFrame(data={"approach": "LHS", "num_snapshots": num_snapshots, 
-                                            "error": self.y_axis_quantity(tmp)})
+                df_tmp = pd.DataFrame(data={"approach": "LHS", "num_snapshots": num_snapshots, "E_MeV": self.E_MeV,
+                                            "error": self.y_axis_quantity(tmp), "emulator_type": self.emulator_type})
                 df = pd.concat((df, df_tmp))
         return df if df_out is None else pd.concat((df_out, df))
 
@@ -178,38 +179,38 @@ class convergenceAnalysis:
             #     print(num_snapshots, len(emu.included_snapshots_idxs))
             #     assert num_snapshots == len(emu.included_snapshots_idxs), "mismatching number of included snapshots [new]"
             #     tmp = emu.emulate(self.lecs_list_validation, mode=self.emulator_type, which=self.which)
-            #     df_tmp = pd.DataFrame(data={"approach": emu.label, "num_snapshots": num_snapshots, 
+            #     df_tmp = pd.DataFrame(data={"approach": emu.label, "num_snapshots": num_snapshots, "E_MeV": self.E_MeV,
             #                                 "error": self.y_axis_quantity(tmp)})
             #     df = pd.concat((df, df_tmp))
             
             for emu in self.greedy_emulators:
                 assert num_snapshots == len(emu.included_snapshots_idxs), "mismatching number of included snapshots"
                 tmp = emu.emulate(self.lecs_list_validation, mode=self.emulator_type, which=self.which)
-                df_tmp = pd.DataFrame(data={"approach": emu.label, "num_snapshots": num_snapshots, 
-                                            "error": self.y_axis_quantity(tmp)})
+                df_tmp = pd.DataFrame(data={"approach": emu.label, "num_snapshots": num_snapshots, "E_MeV": self.E_MeV,
+                                            "error": self.y_axis_quantity(tmp), "emulator_type": self.emulator_type})
                 df = pd.concat((df, df_tmp))
                 emu.greedy_algorithm(req_num_iter=1)
         return df if df_out is None else pd.concat((df_out, df))
     
 
-def convergenceFig(df_E_MeV_arr, E_MeV_arr, emulator_type):
+def convergenceFig(df_res, E_MeV_arr, emulator_type):
     import matplotlib.ticker as ticker 
     import seaborn as sns
     from constants import cm
     fig, axs = plt.subplots(2, 1, figsize=(8.6*cm,12.6*cm), 
                             sharex=True, sharey=True, constrained_layout=True)
 
-    for idf, df in enumerate(df_E_MeV_arr):
-        df = df[df["emulator_type"] == emulator_type]
-        ax=axs[idf]
+    for iE_MeV, E_MeV in enumerate(E_MeV_arr):
+        df = df_res[(df_res["emulator_type"] == emulator_type) & (df_res["E_MeV"] == E_MeV)]
+        ax = axs[iE_MeV]
         # sns.violinplot(data=df, x="num_snapshots", y="error", hue="approach",
         #             gap=.15, split=True, inner="quart", ax=ax, fill=True,
-        #             legend=("auto" if idf==0 else False))
+        #             legend=("auto" if iE_MeV==0 else False))
         sns.boxplot(data=df, x="num_snapshots", y="error", hue="approach",
                     gap=.25, ax=ax, fill=True, width=.8, log_scale=True,
                     showfliers = False,
                     whis=(1, 99), legend="auto")
-        if idf == 0:
+        if iE_MeV == 0:
             ax.set_title("relative error in $p/K$")
         ax.xaxis.set_minor_locator(plt.NullLocator())
     #     ax.set_xlim(1.5,12.5)
@@ -219,13 +220,14 @@ def convergenceFig(df_E_MeV_arr, E_MeV_arr, emulator_type):
         # ax.set_yscale('log')
         # ax.set_ylabel(r"base-10 logarithmic relative error in $|p/K|$")
         ax.set_ylabel("")
-        ax.text(0.7, 0.68, f"$E = {E_MeV_arr[idf]}" + "\; \mathrm{MeV}$ ", 
+        ax.text(0.7, 0.68, f"$E = {E_MeV_arr[iE_MeV]}" + "\; \mathrm{MeV}$ ", 
                 transform=ax.transAxes)
-        ax.text(0.05, 0.07, f"{emulator_type.upper()}", 
+        emulator_type_lbl = {"GROM": "G-ROM", "LSPG": "LSPG-ROM"}
+        ax.text(0.05, 0.07, f"{emulator_type_lbl[emulator_type.upper()]}", 
                 transform=ax.transAxes)
         # ax.text(0.05, 0.05, "base-10 logarithmic relative error in $|p/K|$", 
         #         transform=ax.transAxes)
-        # if idf == 0:
-        ax.legend(ncol=2, fontsize=7, handlelength=2)
+        # if iE_MeV == 0:
+        ax.legend(ncol=2, loc="upper right", fontsize=7, handlelength=2)
         # plt.ylim(bottom=1e-9)
         fig.savefig(f"convergence_minnesota_{emulator_type}_logaxis_symerror.pdf")
